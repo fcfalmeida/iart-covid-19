e3bffe158ecda4d3f9af44a924c523e3f8ce629d
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-04-28 11:34:40 +0100

    add prep fillna toolbox command

--- toolbox/__main__.py
+++ toolbox/__main__.py
@@ -3,7 +3,7 @@ import toolbox.utils as utils
 import toolbox.handlers as handlers
 
 parser = argparse.ArgumentParser()
-subparsers = parser.add_subparsers(help='sub-command help')
+subparsers = parser.add_subparsers(help='command help')
 
 parser_train = subparsers.add_parser('train')
 parser_train.set_defaults(func=handlers.handle_train)
@@ -24,6 +24,18 @@ parser_predict.add_argument('model_file', help='Path to a file which contains a 
 parser_predict.add_argument('target_col', help='Name of the column to predict')
 parser_predict.add_argument('-o', '--outfile', dest='out_file',
     help='When specified, saves the predictions on a .csv file. Takes as argument the name of the file')
+
+parser_prep = subparsers.add_parser('prep')
+prep_subparsers = parser_prep.add_subparsers(help='prep help')
+
+parser_prep_fillna = prep_subparsers.add_parser('fillna')
+parser_prep_fillna.set_defaults(func=handlers.handle_prep_fillna)
+parser_prep_fillna.add_argument('file', help='Path to the .csv file on which to fill the NA values')
+parser_prep_fillna.add_argument('column', help='Name of the column for which NA values should be filled')
+parser_prep_fillna.add_argument('expression', help='Valid Python expression by which NA values should be replaced. \
+    If it contains spaces, it must be surrounded by double quotes')
+parser_prep_fillna.add_argument('-o', '--outfile', dest='out_file',
+    help='When specified, saves the transformed dataframe on a .csv file. Takes as argument the name of the file')
 
 args = parser.parse_args()
 args.func(args)
--- toolbox/handlers.py
+++ toolbox/handlers.py
@@ -49,3 +49,13 @@ def handle_predict(args):
     if args.out_file:
         df.to_csv(args.out_file, index=False)
         print('Output saved in {0} file'.format(args.out_file))
+
+def handle_prep_fillna(args):
+    df = pd.read_csv(args.file)
+
+    df[args.column] = df[args.column].fillna(eval(args.expression))
+    print(df)
+
+    if args.out_file:
+        df.to_csv(args.out_file, index=False)
+        print('Output saved in {0} file'.format(args.out_file))
