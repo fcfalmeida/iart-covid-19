e19a0c852b567ecf9be0c42757d29ec49c7fae34
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-04-29 13:59:44 +0100

    add KNeighborsRegressor

--- toolbox/algorithms.py
+++ toolbox/algorithms.py
@@ -1,10 +1,12 @@
 from enum import Enum
 from sklearn.ensemble import RandomForestRegressor as RFR
 from sklearn.neural_network import MLPRegressor as MLPR
+from sklearn.neighbors import KNeighborsRegressor as KNR
 
 class Algorithms(Enum):
     RandomForestRegressor = RFR()
     MLPRegressor = MLPR()
+    KNeighborsRegressor = KNR()
 
     def __str__(self):
         return self.name
